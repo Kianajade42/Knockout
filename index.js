e9edb4b91864429873b77c1ee6637dbb49ab686d
Kiana Long <Kianajade42.kl@gmail.com>
2022-12-03 10:07:02 -0500

    project set up refactor/imgs

--- index.js
+++ index.js
@@ -6,63 +6,8 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
-class Player {
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position 
-        this.velocity = velocity 
-        this.width = 50
-        this.height = 150 
-        this.lastKey
-        this.color = color
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-              width: 100,
-              height: 50
-        }
-        this.isAttacking
-        this.health = 100
-    }
 
-    draw() {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-       
-        if (this.isAttacking){
-            c.fillStyle = 'green'
-        c.fillRect(this.attackBox.position.x,
-             this.attackBox.position.y, 
-             this.attackBox.width, 
-             this.attackBox.height)
-        }
-        
-    
-    }
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-        } else  this.velocity.y += gravity
-    }
-
-    attack(){
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-}
-
-  const playerOne = new Player({
+  const playerOne = new Character({
     position: {
     x: 0,
     y: 0
@@ -79,7 +24,7 @@ offset: {
 })
 
 
- const playerTwo = new Player({
+ const playerTwo = new Character({
     position:{
     x: 400,
     y: 100
